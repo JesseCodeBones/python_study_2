d0423ab49bcfd94444e0cccadbcab6b2dde2ccc2
Jesse Chen <ruixiang.chen@partner.bmw.com>
2023-04-19 14:14:50 +0800

    update

---
+++ numpy_1.py
@@ -0,0 +1,83 @@
+import numpy as np
+a = np.array([1,2,3])
+print(a)
+
+a = np.array([1, 2, 3, 4, 5], ndmin =  2)  
+print(a)
+
+a = np.array([1,  2,  3], dtype = float)  
+print (a)
+
+## 一，类型
+
+# 名称	描述
+# bool_	布尔型数据类型（True 或者 False）
+# int_	默认的整数类型（类似于 C 语言中的 long，int32 或 int64）
+# intc	与 C 的 int 类型一样，一般是 int32 或 int 64
+# intp	用于索引的整数类型（类似于 C 的 ssize_t，一般情况下仍然是 int32 或 int64）
+# int8	字节（-128 to 127）
+# int16	整数（-32768 to 32767）
+# int32	整数（-2147483648 to 2147483647）
+# int64	整数（-9223372036854775808 to 9223372036854775807）
+# uint8	无符号整数（0 to 255）
+# uint16	无符号整数（0 to 65535）
+# uint32	无符号整数（0 to 4294967295）
+# uint64	无符号整数（0 to 18446744073709551615）
+# float_	float64 类型的简写
+# float16	半精度浮点数，包括：1 个符号位，5 个指数位，10 个尾数位
+# float32	单精度浮点数，包括：1 个符号位，8 个指数位，23 个尾数位
+# float64	双精度浮点数，包括：1 个符号位，11 个指数位，52 个尾数位
+# complex_	complex128 类型的简写，即 128 位复数
+# complex64	复数，表示双 32 位浮点数（实数部分和虚数部分）
+# complex128	复数，表示双 64 位浮点数（实数部分和虚数部分）
+
+# int8, int16, int32, int64 四种数据类型可以使用字符串 'i1', 'i2','i4','i8' 代替
+dt = np.dtype('i4')
+print(dt)
+
+dt = np.dtype([('age',np.int8)]) 
+print(dt)
+
+dt = np.dtype([('age', np.int8), ('type', np.int8)]) 
+a = np.array([(10,12,),(20,12,),(30,12,)], dtype = dt) 
+print(a['age'])
+print(a['type'])
+
+student = np.dtype([('name','S20'), ('age', 'i1'), ('marks', 'f4')]) 
+a = np.array([('abc', 21, 50),('xyz', 18, 75)], dtype = student) 
+print(a['name'])
+
+
+## 二，数组属性
+
+# 属性	说明
+# ndarray.ndim	秩，即轴的数量或维度的数量
+# ndarray.shape	数组的维度，对于矩阵，n 行 m 列
+# ndarray.size	数组元素的总个数，相当于 .shape 中 n*m 的值
+# ndarray.dtype	ndarray 对象的元素类型
+# ndarray.itemsize	ndarray 对象中每个元素的大小，以字节为单位
+# ndarray.flags	ndarray 对象的内存信息
+# ndarray.real	ndarray元素的实部
+# ndarray.imag	ndarray 元素的虚部
+# ndarray.data	包含实际数组元素的缓冲区，由于一般通过数组的索引获取元素，所以通常不需要使用这个属性。
+
+a = np.arange(24)
+print(a)
+b = a.reshape(2,3,4)
+print(b)
+
+a = np.array([[1,2,3],[4,5,6]])  
+print (a.shape)
+
+a = np.array([[1,2,3],[4,5,6]]) 
+b = a.reshape(3,2)  
+print (b)
+
+# itemsize 
+# 数组的 dtype 为 int8（一个字节）  
+x = np.array([1,2,3,4,5], dtype = np.int8)  
+print (x.itemsize)
+ 
+# 数组的 dtype 现在为 float64（八个字节） 
+y = np.array([1,2,3,4,5], dtype = np.float64)  
+print (y.itemsize)
